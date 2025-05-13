65a7adb7e946bf9d244c1d5df66071595d60c4ba
Massimo Ridella <mridella.ieu2022@student.ie.edu>
2025-05-13 20:33:44 +0200

    clean up

--- iebank_api/models.py
+++ iebank_api/models.py
@@ -5,12 +5,13 @@ import string, random
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False)
+    country = db.Column(db.String(32), nullable = False)
     account_number = db.Column(db.String(20), nullable=False, unique=True)
     balance = db.Column(db.Float, nullable=False, default = 0.0)
     currency = db.Column(db.String(1), nullable=False, default="€")
     status = db.Column(db.String(10), nullable=False, default="Active")
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    country = db.Column(db.String(32), nullable = False)
+    
 
     def __repr__(self):
         return '<Event %r>' % self.account_number
