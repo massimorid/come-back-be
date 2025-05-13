ee402d515b9b38ebe35f066f55eb003de2e96a4c
Massimo Ridella <mridella.ieu2022@student.ie.edu>
2025-05-13 20:11:37 +0200

    Adding country fields to models (data model) and routes (when creating account)

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -30,7 +30,8 @@ def skull():
 def create_account():
     name = request.json['name']
     currency = request.json['currency']
-    account = Account(name, currency)
+    country = country.json['country']
+    account = Account(name, currency, country)
     db.session.add(account)
     db.session.commit()
     return format_account(account)
@@ -67,5 +68,6 @@ def format_account(account):
         'balance': account.balance,
         'currency': account.currency,
         'status': account.status,
-        'created_at': account.created_at
+        'created_at': account.created_at,
+        'country': account.country
     }
